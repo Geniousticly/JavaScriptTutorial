091fea20be29e7a2e3061e98bf77f67636cd3702
Geniousticly <95272866+Geniousticly@users.noreply.github.com>
2021-12-13 15:59:00 +0330

    Creation

---
+++ First.js
@@ -0,0 +1,15 @@
+class Me {
+
+  constructor(name, aikh) {
+
+    this.name = name;
+    this.aikh = aikh;
+
+  }
+}
+
+let MehdiNikopour = new Me("MehdiNikopour", "Taikh");
+
+console.log(MehdiNikopour.aikh);
+
+console.log(Math.pow(2, 3));
