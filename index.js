b2d4aa8d9883b421811ff72e7694674554b7c71e
Geniousticly <95272866+Geniousticly@users.noreply.github.com>
2021-12-13 17:05:47 +0330

    TutorialStart

---
+++ index.js
@@ -0,0 +1,8 @@
+var age = 21;
+var firstName = "Bro";
+var lastName = "Code"
+var online = true;
+
+console.log("Your age is ", age);
+console.log("Hello", firstName);
+console.log("Are you online?:", online);
