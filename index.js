343d399019bfc1ed3c3671e87343047fa8a9c9b9
Geniousticly <95272866+Geniousticly@users.noreply.github.com>
2021-12-14 09:08:54 +0330

    Last

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ var age = 21;
 var firstName = "Bro";
 var lastName = "Code"
 var online = true;
+var whatever = null;
+
+// let, const
+
+age++;
+
+var fullName = firstName + lastName;
 
 console.log("Your age is ", age);
-console.log("Hello", firstName);
+console.log("Hello", fullName);
 console.log("Are you online?:", online);
+console.log(whatever);
